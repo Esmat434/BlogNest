412da208194e14e753720a4ded56770ec7a8ca72
alex <alex@gmail.com>
2025-02-14 06:10:13 -0800

    initial commit

--- core/settings.py
+++ core/settings.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from decouple import config
 from django_recaptcha.fields import ReCaptchaField
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -87,10 +89,7 @@ ASGI_APPLICATION = 'core.asgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='postgres://...')
 }
 
 
